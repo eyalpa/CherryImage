7918d41703b513f82aecafbfa9946f739f5cbf85
Eyal Pano <eyal.pano@gmail.com>
2024-03-02 20:42:29 +0200

    update version

--- CherryUI/src/app/three-viewer/three-viewer.component.ts
+++ CherryUI/src/app/three-viewer/three-viewer.component.ts
@@ -9,8 +9,8 @@ import {
   HostListener,
 } from '@angular/core';
 import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 @Component({
   selector: 'app-three-viewer',
