2a6b23ab38ed4e943c0e57b4a9c616dabe919295
Eyal Pano <eyal.pano@gmail.com>
2024-03-06 11:30:48 +0200

    lambda change

--- CherryImage/Function.cs
+++ CherryImage/Function.cs
@@ -1,117 +1,101 @@
-using Amazon.Lambda.Core;
-using Amazon.Lambda.APIGatewayEvents;
 using Amazon.S3;
 using Amazon.S3.Model;
-using System;
 using System.Text;
-using System.Threading.Tasks;
-using System.Collections.Generic;
-using System.IO;
-using Amazon.S3.Util;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.APIGatewayEvents;
 
-[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
-
+// Lambda entry point
 public class Function
 {
-    private readonly IAmazonS3 _s3Client;
-    private const string BucketName = "cherryi";
-
-    public Function()
-    {
-        _s3Client = new AmazonS3Client();
-    }
+    private static readonly AmazonS3Client s3Client = new AmazonS3Client();
+    private const string bucketName = "cherryi";
 
     public async Task<APIGatewayProxyResponse> FunctionHandler(APIGatewayProxyRequest request, ILambdaContext context)
     {
         try
         {
-            var objectKey = request.QueryStringParameters != null && request.QueryStringParameters.ContainsKey("file") ?
-                            request.QueryStringParameters["file"] : string.Empty;
-
+            string objectKey = request.QueryStringParameters?["file"] ?? "";
             switch (request.HttpMethod.ToUpper())
             {
                 case "GET":
-                    context.Logger.LogLine($"GET: objectKey:{objectKey}");
-                    if (request.Path.EndsWith("/CherryImageNodeJS") && string.IsNullOrEmpty(objectKey))
+                    context.Logger.LogLine($"GET : objectKey:{objectKey} event:{request}");
+                    if (request.Path.EndsWith("/CherryImage") && string.IsNullOrEmpty(objectKey))
                     {
-                        var fileList = await ListFilesInBucket();
-                        return new APIGatewayProxyResponse
-                        {
-                            StatusCode = 200,
-                            Body = System.Text.Json.JsonSerializer.Serialize(fileList)
-                        };
+                        var fileList = await ListFilesInBucketAsync();
+                        return CreateResponse(200, fileList);
                     }
                     else
                     {
-                        var url = await GeneratePreSignedURL(objectKey);
-                        return new APIGatewayProxyResponse
-                        {
-                            StatusCode = 200,
-                            Body = System.Text.Json.JsonSerializer.Serialize(new { PreSignedUrl = url })
-                        };
+                        string requestMethod = request.QueryStringParameters?["method"] ?? "GET";
+                        var url = GeneratePreSignedURL(objectKey, requestMethod);
+                        return CreateResponse(200, new { PreSignedUrl = url });
                     }
 
                 case "POST":
-                    context.Logger.LogLine($"POST: objectKey:{objectKey}");
-                    var decodedBody = Convert.FromBase64String(request.Body);
-                    await UploadFileToBucket(objectKey, new MemoryStream(decodedBody));
-                    return new APIGatewayProxyResponse
-                    {
-                        StatusCode = 200,
-                        Body = "File uploaded successfully"
-                    };
+                    context.Logger.LogLine($"POST : objectKey:{objectKey}, {request.IsBase64Encoded} {request.Body}");
+                    byte[] decodedBody = request.IsBase64Encoded ? Convert.FromBase64String(request.Body) : Encoding.UTF8.GetBytes(request.Body);
+                    var gltfContent = ExtractGLTFContent(decodedBody);
+                    await UploadFileToBucketAsync(objectKey, gltfContent);
+                    return CreateResponse(200, "File uploaded successfully");
 
                 default:
-                    context.Logger.LogLine("Method Not Allowed");
-                    return new APIGatewayProxyResponse
-                    {
-                        StatusCode = 405,
-                        Body = "Method Not Allowed"
-                    };
+                    context.Logger.LogLine($"Default : event:{request}");
+                    return CreateResponse(405, "Method Not Allowed");
             }
         }
         catch (Exception e)
         {
             context.Logger.LogLine($"Error: {e.Message}");
-            return new APIGatewayProxyResponse
-            {
-                StatusCode = 500,
-                Body = $"Internal server error: {e.Message}"
-            };
+            return CreateResponse(500, $"Internal server error: {e.Message}");
         }
     }
 
-    private async Task<List<string>> ListFilesInBucket()
+    private static APIGatewayProxyResponse CreateResponse(int statusCode, object body)
     {
-        var response = await _s3Client.ListObjectsV2Async(new ListObjectsV2Request
+        return new APIGatewayProxyResponse
         {
-            BucketName = BucketName
-        });
-
-        return response.S3Objects != null ? response.S3Objects.ConvertAll(obj => obj.Key) : new List<string>();
+            StatusCode = statusCode,
+            Body = System.Text.Json.JsonSerializer.Serialize(body),
+            Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
+        };
     }
 
-    private async Task<string> GeneratePreSignedURL(string objectKey)
+    private static byte[] ExtractGLTFContent(byte[] content)
     {
-        var request = new GetPreSignedUrlRequest
+        string contentAsString = Encoding.UTF8.GetString(content);
+        int glTFPosition = contentAsString.IndexOf("glTF");
+        if (glTFPosition != -1)
         {
-            BucketName = BucketName,
-            Key = objectKey,
-            Expires = DateTime.Now.AddHours(1)
-        };
-
-        return _s3Client.GetPreSignedURL(request);
+            return content[glTFPosition..];
+        }
+        else
+        {
+            Console.WriteLine("\"glTF\" marker not found. Returning original content.");
+            return content;
+        }
     }
 
-    private async Task UploadFileToBucket(string objectKey, MemoryStream stream)
+    private static async Task<IEnumerable<string>> ListFilesInBucketAsync()
     {
-        var putRequest = new PutObjectRequest
-        {
-            BucketName = BucketName,
-            Key = objectKey,
-            InputStream = stream
-        };
+        var response = await s3Client.ListObjectsV2Async(new ListObjectsV2Request { BucketName = bucketName });
+        return response.S3Objects?.ConvertAll(obj => obj.Key) ?? new List<string>();
+    }
 
-        await _s3Client.PutObjectAsync(putRequest);
+    private static string GeneratePreSignedURL(string objectKey, string method)
+    {
+        var request = method.ToUpper() == "GET" ? new GetPreSignedUrlRequest { BucketName = bucketName, Key = objectKey, Verb = HttpVerb.GET, Expires = DateTime.UtcNow.AddHours(1) } :
+                                                    new GetPreSignedUrlRequest { BucketName = bucketName, Key = objectKey, Verb = HttpVerb.PUT, Expires = DateTime.UtcNow.AddHours(1) };
+        return s3Client.GetPreSignedURL(request);
+    }
+
+    private static async Task UploadFileToBucketAsync(string objectKey, byte[] content)
+    {
+        var request = new PutObjectRequest
+        {
+            BucketName = bucketName,
+            Key = objectKey,
+            InputStream = new MemoryStream(content),
+        };
+        await s3Client.PutObjectAsync(request);
     }
 }
